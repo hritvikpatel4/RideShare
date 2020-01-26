716d12ac16530cfe4e865b6683f7a2c5bbde1182
Hritvik Patel <hritvik.patel4@gmail.com>
2020-01-26 20:45:56 +0530

    v1.0.1

--- ride_share.py
+++ ride_share.py
@@ -4,46 +4,36 @@ from mysql.connector import Error
 
 ride_share = Flask(__name__)
 
-#API 1: to add a new user to the database.
+# API 1: To add a new user to the database.
 @ride_share.route("/api/v1/users", methods=["PUT"])
 def addUser():
-	#extracting the values from the JSON file sent.
-	parameters = request.get_json()
-	#checking for a valid json request
+	parameters = request.get_json()	
 	if "username" in parameters.keys() and "password" in parameters.keys():
-		
-		# Check if username is present in the database.
 		query = "SELECT * from UserDetails WHERE username='{}';".format(parameters["username"])
 		rows = readDB(query)
 		if len(rows):
-			answer = make_response("User name already exists", 405)
-		
-		# All the checks are done and now return a 201 code (record created).
+			answer = make_response("User already exists", 405)
 		else:
 			query = "INSERT INTO UserDetails VALUES ('{}', '{}');".format(parameters["username"], parameters["password"])
 			modifyDB(query)
-			answer = make_response("User name inserted into the table", 201)
-	
-	# if not present, raise a "bad request error". (Error code = 400)
+			answer = make_response("New user added", 201)
 	else:
 		answer = make_response("", 400)
+	
 	return answer
 
 # API 2: to delete an existing user from the database.
 @ride_share.route("/api/v1/users/<username>", methods=["DELETE"])
 def removeUser(username):
-	# Check if username is present in the database.
 	query = "SELECT * from UserDetails WHERE username='{}';".format(username)
 	rows = readDB(query)
 	if len(rows):
 		query = "DELETE FROM UserDetails WHERE username='{}';".format(username)
 		modifyDB(query)
-		answer = make_response("", 200)
-		# TODO: call the API to delete the username in the database.
-	
-	#if not present, this is an invalid request.
+		answer = make_response("User removed", 200)
 	else:
-		answer = make_response("", 405)
+		answer = make_response("Invalid user", 405)
+	
 	return answer
 
 # API 2: to delete an existing user from the database.
@@ -52,13 +42,11 @@ def removeUserFail():
 	answer = make_response("", 400)
 	return answer
 
-'''
 # API 3: create a new ride
 @ride_share.route("/api/v1/rides", methods=["POST"])
 def newRide():
 	parameters = request.get_json()
-	# if "created_by", "timestamp", "source", "destination" in parameters.keys():
-	if True:	
+	if "created_by" in parameter.keys() and "timestamp" in parameters.keys() and "source" in parameters.keys() and "destination" in parameters.keys():
 		#check db for the username using parameters["created_by"]
 		if #exists in db:
 			# implement the create ride operation on db
@@ -74,7 +62,7 @@ def newRide():
 		#wrong api call
 		answer = make_response("", 400)
 		return answer
-
+'''
 # API 4: List all upcoming rides for a given source and destination
 @ride_share.route("/api/v1/rides?source=<source>&destination=<destination>", methods=["GET"])
 def listRides(source, destination):
