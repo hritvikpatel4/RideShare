ffd8fb449bb27628e46c30bc471dfd3852b663b9
prams628 <prams1999@gmail.com>
2020-01-31 20:54:59 +0530

    Modified the calls to database in APIs 1, 2, 3, 6

--- a1/ride_share.py
+++ a1/ride_share.py
@@ -19,6 +19,42 @@ def get_area_from_number(a):
             line_count+=1
         return "error"
 
+# Function to construct the SQL query
+def construct_query(data):
+	# data is of JSON type
+	# operation is INSERT
+	if data["operation"] == "INSERT":
+		SQLQuery = "INSERT INTO {} ({}".format(data["tablename"], data["columns"][0])
+		for value in range(1, len(data["columns"])):
+			SQLQuery += ",{}".format(data["columns"][value])
+		SQLQuery += ") VALUES ('{}'".format(data["values"][0])
+		for value in range(1, len(data["values"])):
+			SQLQuery += ",'{}'".format(data["values"][value])
+		SQLQuery += ");"
+	
+	# SELECT operation
+	elif data["operation"] == "SELECT":
+		if data["columns"] == "*":
+			SQLQuery = "SELECT *"
+		else: 
+			SQLQuery = "SELECT {}".format(data["columns"][0])
+			for value in range(1, len(data["columns"])):
+				SQLQuery += ",{}".format(data["columns"][value])
+		SQLQuery += " FROM {} WHERE ".format(data["tablename"])
+		for condition in data["where"]:
+			SQLQuery += condition
+		SQLQuery += ";"
+
+	# DELETE operation
+	elif data["operation"] == "DELETE":
+		SQLQuery = "DELETE FROM {}".format(data["tablename"])
+		if "where" in data.keys():
+			SQLQuery += " WHERE "
+			for condition in data["where"]:
+				SQLQuery += condition
+		SQLQuery += ";"
+	return SQLQuery
+
 # API 1: To add a new user to the database.
 @ride_share.route("/api/v1/users", methods=["PUT"])
 def addUser():
@@ -30,15 +66,25 @@ def addUser():
 			if parameters["password"][i] != string.hexdigits:
 				answer = make_response("400 Bad Syntax", 400)
 		
-		query = "SELECT * from userdetails WHERE username='{}';".format(parameters["username"])
-		rows = readDB(query)
+		data = {
+				"operation": "SELECT",
+				"columns": "*",
+				"tablename": "userdetails",
+				"where": ["username='{}'".format(parameters["username"])]
+			}
+		rows = readDB(data)
 		
 		if len(rows):
 			answer = make_response("400 User already exists", 400)
 		
 		else:
-			query = "INSERT INTO userdetails VALUES ('{}', '{}');".format(parameters["username"], parameters["password"])
-			modifyDB(query)
+			data = {
+				"operation": "INSERT",
+				"tablename": "userdetails",
+				"columns": ["username", "password"],
+				"values": [parameters["username"], parameters["password"]]
+			}
+			modifyDB(data)
 			answer = make_response("201 New user added", 201)
 	
 	else:
@@ -49,12 +95,21 @@ def addUser():
 # API 2: To delete an existing user from the database.
 @ride_share.route("/api/v1/users/<username>", methods=["DELETE"])
 def removeUser(username):
-	query = "SELECT * from userdetails WHERE username='{}';".format(username)
-	rows = readDB(query)
+	data = {
+				"operation": "SELECT",
+				"columns": "*",
+				"tablename": "userdetails",
+				"where": ["username='{}'".format(username)]
+		}
+	rows = readDB(data)
 
 	if len(rows):
-		query = "DELETE FROM userdetails WHERE username='{}';".format(username)
-		modifyDB(query)
+		data = {
+			"operation": "DELETE",
+			"tablename": "userdetails",
+			"where": ["username='{}'".format(username)]
+		}
+		modifyDB(data)
 		answer = make_response("200 User removed", 200)
 	
 	else:
@@ -68,16 +123,26 @@ def newRide():
 	parameters = request.get_json()
 
 	if "created_by" in parameters.keys() and "timestamp" in parameters.keys() and "source" in parameters.keys() and "destination" in parameters.keys():
-		query1 = "SELECT * from userdetails WHERE username='{}';".format(parameters["created_by"])
-		rows = readDB(query1)
+		data = {
+				"operation": "SELECT",
+				"columns": "*",
+				"tablename": "userdetails",
+				"where": ["username='{}'".format(parameters["created_by"])]
+		}
+		rows = readDB(data)
 		
 		if len(rows):
 			date, time = parameters["timestamp"].split(":")
 			ss, mm, hh = time.split("-")
 			dd, mo, yy = date.split("-")
 			timestamp = "{}-{}-{} {}:{}:{}".format(yy, mo, dd, hh, mm, ss)
-			query2 = "INSERT INTO ridedetails (created_by, timestamp, source, destination) VALUES ('{}', '{}', '{}', '{}');".format(parameters["created_by"], timestamp, parameters["source"], parameters["destination"])
-			modifyDB(query2)
+			data = {
+				"operation": "INSERT",
+				"tablename": "ridedetails",
+				"columns": ["created_by", "timestamp", "source", "destination"],
+				"values": [parameters["created_by"], parameters["timestamp"], parameters["source"], parameters["destination"]]
+			}
+			modifyDB(data)
 			answer = make_response("200 Ride successfully created", 200)
 		
 		else:
@@ -162,29 +227,58 @@ def joinRide(rideId):
 
 	if rideId and parameters["username"] and "username" in parameters.keys():
 		query = "SELECT rideid FROM ridedetails WHERE rideid = '{}';".format(rideId)
-		rows_ride = readDB(query)
-		if not rows_ride:
-			return make_response("400 Invalid ride ID", 400)
+		data = {
+				"operation": "SELECT",
+				"columns": ["rideid"],
+				"tablename": "ridedetails",
+				"where": ["rideid={}".format(rideId)]
+		}
+		rows_ride = readDB(data)
 		
-		query = "SELECT username FROM userdetails WHERE username = '{}';".format(parameters["username"])
-		rows_user = readDB(query)
+		if not rows_ride:
+			return make_response("Invalid ride ID", 405)
+		
+		data = {
+				"operation": "SELECT",
+				"columns": ["username"],
+				"tablename": "userdetails",
+				"where": ["username='{}'".format(parameters["username"])]
+		}
+		rows_user = readDB(data)
 		if not rows_user:
 			return make_response("400 Invalid username", 400)
 		
-		query = "SELECT created_by FROM ridedetails WHERE rideid = '{}';".format(rideId)
-		verify_user1 = readDB(query)
+		data = {
+				"operation": "SELECT",
+				"columns": ["created_by"],
+				"tablename": "ridedetails",
+				"where": ["rideid={}".format(rideId)]
+		}
+		verify_user1 = readDB(data)
 		verify_user1 = [x[0] for x in verify_user1]
 		if parameters["username"] in verify_user1:
 			return make_response("400 Cannot join a ride created by yourself", 400)
 		
-		query = "SELECT username FROM rideusers WHERE rideid = '{}';".format(rideId)
-		verify_user2 = readDB(query)
+		data = {
+				"operation": "SELECT",
+				"columns": ["username"],
+				"tablename": "rideusers",
+				"where": ["rideid={}".format(rideId)]
+		}
+		verify_user2 = readDB(data)
 		verify_user2 = [x[0] for x in verify_user2]
 		if parameters["username"] in verify_user2:
 			return make_response("400 You have already joined the ride", 400)
 
+		data = {
+				"operation": "INSERT",
+				"tablename": "rideusers",
+				"columns": ["rideid", "username"],
+				"values": [rideId, parameters["username"]]
+		}
 		query = "INSERT INTO rideusers VALUES ('{}', '{}');".format(rideId, parameters["username"])
-		modifyDB(query)
+		modifyDB(data)
+		
 		answer = make_response("200 Joined ride successfully", 200)
 		return answer
 	
@@ -224,9 +318,11 @@ def connectDB(user, pwd, db):
 
 # API 8: API to modify (insert or delete) values from database
 @ride_share.route("/api/v1/db/write", methods=["POST"])
-def modifyDB(SQLQuery):
+def modifyDB(data):
 	conn = connectDB('root', '', 'ride_share')
 	cursor = conn.cursor()
+	SQLQuery = construct_query(data)
+	print(SQLQuery)
 	cursor.execute(SQLQuery)
 	conn.commit()
 	cursor.close()
@@ -234,9 +330,11 @@ def modifyDB(SQLQuery):
 
 #API 9: API to read values from database
 @ride_share.route("/api/v1/db/read")
-def readDB(SQLQuery):
+def readDB(data):
 	conn = connectDB('root', '', 'ride_share')
 	cursor = conn.cursor()
+	SQLQuery = construct_query(data)
+	print(SQLQuery)
 	cursor.execute(SQLQuery)
 	rows = cursor.fetchall()
 	cursor.close()
